836b71551b6d719f17a54f649361b26c8f6354c1
takakiono <86759770+takakiono@users.noreply.github.com>
2021-07-03 11:52:23 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+$(function(){
+    //login-modalを表示
+    $('#login-show').click(function(){
+        $('#login-modal').fadeIn();
+    });
+
+    //modalを閉じる
+    $('.close-modal').click(function(){
+        $('#signup-modal').fadeOut();
+        $('#login-modal').fadeOut();
+    });
+
+    //signup-modalを表示
+    $('.signup-show').click(function(){
+        $('#signup-modal').fadeIn();
+    });
+
+    //lessonのhover処理
+    $('.lesson').hover(
+        //カーソルが乗っているとき
+        function(){
+            $(this).find('.text-contents').addClass('text-active');
+        },
+        //カーソルが外れているとき
+        function(){
+            $(this).find('.text-contents').removeClass('text-active');
+        }
+    );
+    
+    //FAQ クリックによる表示・非表示（アコーディオン）
+    $('.faq-list-item').click(function(){
+        var $answer = $(this).find('.answer');
+
+        //表示
+        if($answer.hasClass('open')){
+            $($answer).removeClass('open');
+            $($answer).slideUp();
+            $(this).find('span').text('+');
+        //非表示
+        }else{
+            $($answer).addClass('open');
+            $($answer).slideDown();
+            $(this).find('span').text('-');
+        }
+    });
+    
+});
